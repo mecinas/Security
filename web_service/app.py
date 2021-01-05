c228c803980670ce6e9b25f9d50882176a6a2727
mecinas <szymon.mecina@vp.pl>
2021-01-05 14:53:15 +0100

    Refactoring

--- web_service/app.py
+++ web_service/app.py
@@ -33,7 +33,8 @@ def allowed_methods(methods):
     response = make_response('', 204)
 
     origin = request.headers.get('Origin')
-    allowed_origins = ["http://localhost:8000", "http://localhost:8000/"]
+    allowed_origins = ["http://localhost:8000",
+                       "http://localhost:8000/", "https://localhost:8000"]
     if origin in allowed_origins:
         response.headers["Access-Control-Allow-Origin"] = origin
 
@@ -48,15 +49,8 @@ def root():
     if request.method == 'OPTIONS':
         return allowed_methods(['GET'])
 
-    links = []
-    links.append({"type": 'register', "url": '/sender/register'})
-    links.append({"type": 'login', "url": '/sender/login'})
-
-    document = {
-        "data": {},
-        "links": links
-    }
-    return document
+    data = {}
+    return data
 
 
 @app.route("/root/sender/register", methods=['GET', 'OPTIONS'])
@@ -64,13 +58,8 @@ def open_register():
     if request.method == 'OPTIONS':
         return allowed_methods(['GET', 'POST'])
 
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
-    document = {
-        "data": {},
-        "links": links
-    }
-    return document
+    data = {}
+    return data
 
 
 @app.route("/root/sender/login", methods=['GET', 'OPTIONS'])
@@ -78,199 +67,153 @@ def open_login():
     if request.method == 'OPTIONS':
         return allowed_methods(['GET', 'POST'])
 
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
-    document = {
-        "data": {},
-        "links": links
-    }
-    return document
+    data = {}
+    return data
+
+def authorizate(data, token):
+    try:
+        g.authorization = decode(token, JWT_SECRET, algorithms=['HS256'])
+    except Exception as e:
+        return False
+    if not is_login(g.authorization.get('login')):
+        return False
+    data["is_authorized"] = True
+    return True
 
 @app.route("/root/sender/changePswd", methods=['GET', 'OPTIONS'])
 def open_changePswd():
     if request.method == 'OPTIONS':
         return allowed_methods(['GET', 'POST'])
 
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
     data = {'is_authorized': False}
-    document = {
-        "data": data,
-        "links": links
-    }
     token = request.headers.get("cookie")
-    try:
-        g.authorization = decode(token, JWT_SECRET, algorithms=['HS256'])
-    except Exception as e:
-        return document
-    if not is_login(g.authorization.get('login')):
-        return document
-    data["is_authorized"] = True
+    authorizate(data, token)
 
-    return document
+    return data
+
 
 @app.route("/root/sender/control/answer", methods=['GET', 'OPTIONS'])
 def open_check_control_answer():
     if request.method == 'OPTIONS':
         return allowed_methods(['GET', 'POST'])
 
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
     data = {'is_authorized': False}
-    document = {
-        "data": data,
-        "links": links
-    }
     token = request.headers.get("cookie")
-    try:
-        g.authorization = decode(token, JWT_SECRET, algorithms=['HS256'])
-    except Exception as e:
-        return document
-    if not is_login(g.authorization.get('login')):
-        return document
-    data["is_authorized"] = True
+    authorizate(data, token)
 
-    return document
+    return data
+
 
 @app.route("/root/sender/addNotes", methods=['GET', 'OPTIONS'])
 def open_notes():
     if request.method == 'OPTIONS':
         return allowed_methods(['GET', 'POST'])
 
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
     data = {'is_authorized': False}
-    document = {
-        "data": data,
-        "links": links
-    }
     token = request.headers.get("cookie")
-    try:
-        g.authorization = decode(token, JWT_SECRET, algorithms=['HS256'])
-    except Exception as e:
-        return document
-    if not is_login(g.authorization.get('login')):
-        return document
-    data["is_authorized"] = True
+    authorizate(data, token)
 
-    return document
+    return data
+
 
 @app.route("/root/sender/recoverPswd", methods=['GET', 'OPTIONS'])
 def open_recoverPswd():
     if request.method == 'OPTIONS':
         return allowed_methods(['GET', 'POST'])
 
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
-    document = {
-        "data": {},
-        "links": links
-    }
+    data = {}
+    return data
 
-    return document
 
 @app.route("/root/sender/publicNotes", methods=['GET', 'OPTIONS'])
 def show_public_notes():
     if request.method == "OPTIONS":
         return allowed_methods(['GET'])
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
+
     data = {
-        "is_authorized": True,
         "has_notes": False,
         "notes": None,
     }
-    document = {
-        "data": data,
-        "links": links
-    }
     notes = db.hget(f"notes", "public")
     if notes is None:
-        return document
+        return data
     else:
         notes = json.loads(notes).get("notes")
         if notes is None:
-            return document
+            return data
 
     data["has_notes"] = True
     data["notes"] = notes
-    return document
+    return data
+
 
 @app.route("/root/sender/privateNotes", methods=['GET', 'OPTIONS'])
 def show_private_notes():
     if request.method == "OPTIONS":
         return allowed_methods(['GET'])
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
+
     data = {
         "is_authorized": False,
         "has_notes": False,
         "notes": None,
     }
-    document = {
-        "data": data,
-        "links": links
-    }
     token = request.headers.get("cookie")
-    try:
-        g.authorization = decode(token, JWT_SECRET, algorithms=['HS256'])
-    except Exception as e:
-        return document
-    login = g.authorization.get('login')
-    if not is_login(login):
-        return document
-    data["is_authorized"] = True
-    
+    if not authorizate(data, token):
+        return data
 
     notes = db.hget(f"user:{login}", "private_notes")
     if notes is None:
-        return document
+        return data
     else:
         notes = json.loads(notes).get("notes")
         if notes is None:
-            return document
+            return data
 
     data["has_notes"] = True
     data["notes"] = notes
-    return document
+    return data
+
 
 @app.route('/root/check/<login>', methods=['GET', 'OPTIONS'])
 def check_login(login):
     if request.method == 'OPTIONS':
         return allowed_methods(['GET', 'POST'])
 
-    result = {login: "available"}
+    data = {login: "available"}
     if is_login(login):
-        result = {login: "taken"}
+        data = {login: "taken"}
 
-    response = make_response(result, 200)
+    response = make_response(data, 200)
     origin = request.headers.get('Origin')
     allowed_origins = ["http://localhost:8000", "http://localhost:8000/"]
     if origin in allowed_origins:
         response.headers["Access-Control-Allow-Origin"] = origin
+
     return response
+
 
 @app.route('/root/recover/check/<login>', methods=['GET', 'OPTIONS'])
 def check_recover_login(login):
     if request.method == 'OPTIONS':
         return allowed_methods(['GET', 'POST'])
 
-    document = {
+    data = {
         "availability": "taken",
         "cookie": None,
         "question": None
     }
     if not is_login(login):
-        document["availability"] = "available"
-        return document
+        data["availability"] = "available"
+        return data
 
     payload = {
         "login": login,
         "access_granted": True
     }
-    document["cookie"] = jwt.encode(payload, JWT_SECRET, algorithm='HS256')
-    document["question"] = db.hget(f"user:{login}", "question")
-    return document
+    data["cookie"] = jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+    data["question"] = db.hget(f"user:{login}", "question")
+    return data
+
 
 def convert_to_output_packages(all_packages, packages_for_response):
     for (f_key, f_value) in all_packages.items():
@@ -286,7 +229,6 @@ def convert_to_output_packages(all_packages, packages_for_response):
                 }
                 packages_for_response.append(single_package)
 
-
 def is_login(login):
     return db.hexists(f"user:{login}", "password")
 
@@ -298,8 +240,7 @@ def verify_user(login, password):
         return False
     return checkpw(password_encoded, hashed_password_database.encode('utf-8'))
 
-
-def save_user(firstname, lastname,login, password, question, answer):
+def save_user(firstname, lastname, login, password, question, answer):
     salt = gensalt(5)
     password_encoded = password.encode()
     hashed_password = hashpw(password_encoded, salt)
@@ -312,41 +253,37 @@ def save_user(firstname, lastname,login, password, question, answer):
     db.hset(f"user:{login}", "question", question)
     db.hset(f"user:{login}", "answer", hashed_answer)
 
+
 def validate_input(firstname, lastname, login, password, sec_password, question, answer):
     valid_input = False
     data = {
         "valid_input": valid_input,
     }
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
-    document = {
-        "data": data,
-        "links": links
-    }
+
     if sec_password != password:
-        return document
+        return data
     if firstname and lastname and login and password:
         if is_login(login):
-            return document
+            return data
 
         save_user(firstname, lastname, login, password, question, answer)
         valid_input = True
         data["valid_input"] = valid_input
-        return document
+        return data
 
-    return document
+    return data
 
 
 @app.route("/root/sender/register", methods=['POST'])
 def register():
-    data = request.json
-    firstname = data.get('firstname')
-    lastname = data.get('lastname')
-    login = data.get('login')
-    password = data.get('password')
-    sec_password = data.get('sec_password')
-    question = data.get('question')
-    answer = data.get('answer')
+    cli_data = request.json
+    firstname = cli_data.get('firstname')
+    lastname = cli_data.get('lastname')
+    login = cli_data.get('login')
+    password = cli_data.get('password')
+    sec_password = cli_data.get('sec_password')
+    question = cli_data.get('question')
+    answer = cli_data.get('answer')
 
     return validate_input(firstname, lastname,
                           login, password, sec_password, question, answer)
@@ -354,28 +291,20 @@ def register():
 
 @app.route("/root/sender/login", methods=['POST'])
 def login():
-    data = request.json
-    login = data.get('login')
-    password = data.get('password')
+    cli_data = request.json
+    login = cli_data.get('login')
+    password = cli_data.get('password')
 
     messages = []
-    is_valid = True
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
     data = {
         "messages": messages,
-        "is_valid": is_valid,
+        "is_valid": True,
         "cookies": None
-    }
-    document = {
-        "data": data,
-        "links": links
     }
     if not verify_user(login, password):
         messages.append("Podano nieprawidłowy login lub hasło")
-        is_valid = False
-        data["is_valid"] = is_valid
-        return document
+        data["is_valid"] = False
+        return data
 
     messages.append("Zalogowano!")
     payload = {
@@ -383,105 +312,86 @@ def login():
         "date": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     }
     cookie = jwt.encode(payload, JWT_SECRET, algorithm='HS256')
-    document["data"]["cookies"] = cookie
-    return document
+    data["cookies"] = cookie
+    return data
+
 
 @app.route("/root/sender/changePswd", methods=['POST'])
 def post_changePswd():
-    data = request.json
-    old_pswd = data.get('old_pswd')
-    new_pswd = data.get('new_pswd')
-    new_pswd2 = data.get('new_pswd2')
+    cli_data = request.json
+    old_pswd = cli_data.get('old_pswd')
+    new_pswd = cli_data.get('new_pswd')
+    new_pswd2 = cli_data.get('new_pswd2')
 
-    links = []
-    links.append({"type": 'root:parent', "url": '/'})
-    data = {'is_authorized': False}
     messages = []
-    document = {
-        'messages':messages,
-        "data": data,
-        "links": links
+    data = {
+        'messages': messages,
+        'is_authorized': False
     }
     token = request.headers.get("cookie")
-    try:
-        g.authorization = decode(token, JWT_SECRET, algorithms=['HS256'])
-    except Exception as e:
-        return document
-    login = g.authorization.get('login')
-    if not is_login(login):
-        return document
-    data["is_authorized"] = True
+    if not authorizate(data, token):
+        return data
 
     if not verify_user(login, old_pswd):
         messages.append("Podane hasło jest nieprawidłowe")
-        return document
+        return data
     if new_pswd != new_pswd2:
         messages.append("Nowe hasła są różne")
-        return document
-    
+        return data
+
+    messages.append("Hasło zostało zmienione")
     salt = gensalt(5)
     password_encoded = new_pswd.encode()
     hashed_password = hashpw(password_encoded, salt)
     db.hset(f"user:{login}", "password", hashed_password)
 
-    return document
-    
+    return data
+
+
 @app.route('/root/sender/validate/answer', methods=['POST'])
 def post_validate_answer():
     answer = request.json.get("answer")
     token = request.headers.get('cookie')
 
-    document = {
-        "data": {
-            "is_authorized": False,
-            "is_valid": False,
-            "cookie":None
-        }
+    data = {
+        "is_authorized": False,
+        "is_valid": False,
+        "cookie": None
     }
-    try:
-        g.authorization = decode(token, JWT_SECRET, algorithms=['HS256'])
-    except Exception as e:
-        return document
-    if not g.authorization.get("access_granted"):
-        return document
-    document["data"]["is_authorized"] = True
+
+    if not authorizate(data, token):
+        return data
     login = g.authorization.get("login")
 
     answer_encoded = answer.encode('utf-8')
     hashed_answer_database = db.hget(
         f"user:{login}", "answer")
     if not hashed_answer_database:
-        return document
+        return data
     if not checkpw(answer_encoded, hashed_answer_database.encode('utf-8')):
-        return document
-    document["data"]["is_valid"] = True
+        return data
+    data["is_valid"] = True
 
     payload = {
         "login": login,
         "is_valid": True
     }
-    document["data"]["cookie"] = jwt.encode(payload, JWT_SECRET, algorithm='HS256')
-    return document
+    data["cookie"] = jwt.encode(
+        payload, JWT_SECRET, algorithm='HS256')
+    return data
+
 
 @app.route('/root/sender/recover/changePswd', methods=['POST'])
 def post_recover_changePswd():
-    password = request.json.get("new_pswd")#Sprawdzenie haseł jest po stronie js
+    password = request.json.get("new_pswd")
     token = request.headers.get('cookie')
 
-    document = {
-        "data": {
-            "is_authorized": False,
-            "is_success": False,
-        }
+    data = {
+        "is_authorized": False,
+        "is_success": False,
     }
-    try:
-        g.authorization = decode(token, JWT_SECRET, algorithms=['HS256'])
-    except Exception as e:
-        return document
-    print(g.authorization)
-    if not g.authorization.get("is_valid"):
-        return document
-    document["data"]["is_authorized"] = True
+    if not authorizate(data, token):
+        return data
     login = g.authorization.get("login")
 
     salt = gensalt(5)
@@ -489,45 +399,44 @@ def post_recover_changePswd():
     hashed_password = hashpw(password_encoded, salt)
 
     db.hset(f"user:{login}", "password", hashed_password)
-    document["data"]["is_success"] = True
+    data["is_success"] = True
 
-    return document
+    return data
+
 
 @app.route('/root/sender/addNotes', methods=['POST'])
 def post_notes():
-    data = request.json
+    cli_data = request.json
     token = request.headers.get('cookie')
+    data = {
+        "is_authorized": False
+    }
+    print("1111")
+    if not authorizate(data, token):
+        print("2222")
+        return data
 
-    try:
-        g.authorization = decode(token, JWT_SECRET, algorithms=['HS256'])
-    except Exception as e:
-        return {'is_authorized': False}
-    login = g.authorization.get('login')
-    if not is_login(login):
-        return {'is_authorized': False}
-    
-    input_note = data.get('note')
+    input_note = cli_data.get('note')
     db_notes = {}
-    if data.get('access') == 'private':
+    if cli_data.get('access') == 'private':
         db_notes = db.hget(f"user{login}", "private_notes")
     else:
         db_notes = db.hget(f"notes", "public")
 
-    new_notes = {"notes":[]}
+    new_notes = {"notes": []}
     if db_notes is None:
         new_notes["notes"].append(input_note)
     else:
         new_notes = json.loads(db_notes)
         new_notes["notes"].append(input_note)
-        
+
     new_notes = json.dumps(new_notes)
-    if data.get('access') == 'private':
-        print(new_notes)
+    if cli_data.get('access') == 'private':
         db.hset(f"user:{login}", 'private_notes', new_notes)
     else:
         db.hset(f"notes", 'public', new_notes)
-
-    return {'is_authorized': True}
+    data["is_authorized"] = True
+    return data
 
 
 if __name__ == '__main__':
